dfcc4e73f4d133fe732ee81eb2179c81e4a27639
victorpenia <victor.pena@ucatec.edu.bo>
2023-08-11 19:12:21 -0400

    Clase cuatro PHP

---
+++ variables.php
@@ -0,0 +1,30 @@
+<?php
+
+$valor = "Bienvenidos al curso de PHP";
+$Valor = 8;
+echo $valor . " " . $Valor;
+
+echo "<br><br>";
+
+$nombre = "Victor";
+$apellido = "Peña";
+$edad = 30;
+$estaMatriculado = true;
+$notaPromedio = 80.5;
+
+echo "Estudiante: " . $nombre . $apellido .  "<br>";
+echo "edad: " . $edad .  "<br>";
+echo "Está matriculado: " . $estaMatriculado .  "<br>";
+echo "Nota promedio: " . $notaPromedio .  "<br>";
+
+
+echo "<br><br>";
+
+$longitud = 15;
+$ancho = 9;
+$area = $longitud * $ancho;
+
+echo "El área del rectángulo es: " . $area;
+
+
+?>
